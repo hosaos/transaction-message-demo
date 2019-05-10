b14a26160c95d097c72b0267b573b315149c4b0b
chenyin <601782332@qq.com>
2019-05-10 23:00:19 +0800

    readme修改

--- src/main/resources/sql/table.sql
+++ src/main/resources/sql/table.sql
@@ -12,8 +12,9 @@ CREATE TABLE `transfer_record` (
   `transaction_id` varchar(128) NOT NULL COMMENT '消息事务id',
   `to_user_id` bigint(20) NOT NULL COMMENT '被转账人id',
   `record_no` varchar(64) NOT NULL COMMENT '转账流水编号',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_record_no` (`record_no`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `user`(`id`, `money`) VALUES (1, 100);
 INSERT INTO `user`(`id`, `money`) VALUES (2, 0);
